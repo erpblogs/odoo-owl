78757b373af54e5272c290a576a805177cd9ce78
quang <quang.trinhvan@vti.com.vn>
2021-12-09 11:28:52 +0700

    Config webpake dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: "main.js",
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        https: false,
+        open: '/',
+        compress: true,
+        hot: true,
+        port: 9999,
+        host: 'localhost',
+        proxy: {
+            '/api': 'http://localhost:9999'
+        },
+        headers: {'Access-Control-Allow-Origin': '*'},
+        // watchFiles: [ '../templates/**/*.*', '../src/**/*.*' ],
+        devMiddleware: {
+            writeToDisk: false,
+        },
+        static: [{
+            directory: path.join(__dirname, 'dist'),
+            serveIndex: true,
+            watch: true,
+        }]
+        // contentBase: path.join(__dirname, 'dist')
     }
 }
