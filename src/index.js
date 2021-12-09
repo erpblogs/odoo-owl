cee879969841741107e610d80988197682ac9786
quang <quang.trinhvan@vti.com.vn>
2021-12-09 18:05:55 +0700

    use materializecss plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import 'bootstrap'
+// import 'bootstrap'
+import 'materialize-css'
+
+// $(document).ready(function(){
+//     $('.carousel').carousel({
+//         interval: 1000,
+//         ride: 'carousel'
+//     })
+// })
 
 $(document).ready(function(){
-    $('.carousel').carousel({
-        interval: 2000,
-        ride: 'carousel'
-    })
-})
+    $('.carousel').carousel();
+
+
+});
