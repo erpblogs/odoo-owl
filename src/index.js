594526f106772ed660425243868e2f2b3a97b4d1
quangtv <quang.trinhvan@vti.com.vn>
2021-12-26 08:10:12 +0700

    html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// import './css/font.css'
 import './css/style.css'
+import './css/home.scss'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path')
 const webpack = require('webpack')
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MediaQueryPlugin = require("media-query-plugin")
+const devMode = process.env.NODE_ENV !== "production"
 
 module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
     entry: {
-        main: ['./src/index', './src/css/style'],
+        main: ['./src/index'],
+        example: './src/example'
     },
     output: {
         filename: '[name].js',
@@ -14,6 +22,7 @@ module.exports = {
         alias: {
             // '@components': path.resolve(__dirname, './src/components'),
             '@components': path.resolve(__dirname, 'src/components/'),
+            '@src': path.resolve(__dirname, 'src/'),
         },
         extensions: ['.js', '.css', '.xml', '.json'],
         // restrictions: [/\.(sass|scss|css)$/],
@@ -32,12 +41,40 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader' ],
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    MediaQueryPlugin.loader,
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ],
+                // options: {
+                //     sourceMap: true,
+                // },
 
             },
             {
+                test: /\.hbs$/,
+                use: [
+                    {
+                        loader: 'handlebars-loader',
+                        options: {
+                            helperDirs: [
+                                path.resolve(__dirname, 'helpers')
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
                 test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                // type: "asset",
                 use: [
                     {
                         loader: 'file-loader',
@@ -59,12 +96,49 @@ module.exports = {
         new ESLintPlugin({
             files: 'src/**/*.js',
             extensions: ['js']
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Media Query Example',
+            filename: 'index.html',
+            template: './src/index.hbs',
+            inject: false,
+            'meta': {
+                'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'News',
+            filename: 'news.html',
+            template: './src/news.html',
+            inject: false,
+            'meta': {
+                'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+            }
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? "[name].css" : "[name].[contenthash].css",
+            chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+        }),
+        new MediaQueryPlugin({
+            include: [
+                'example',
+                'example2'
+            ],
+            queries: {
+                'print, screen and (min-width: 60em)': 'desktop',
+                'print, screen and (min-width: 60em) and (orientation: landscape)': 'desktop'
+            }
         })
     ],
+    optimization: {
+        runtimeChunk: 'single'
+    },
     devServer: {
         https: false,
         // open: '/',
-        compress: true,
+        // compress: true,
         hot: false,
         port: 9999,
         host: 'localhost',
@@ -79,7 +153,7 @@ module.exports = {
         static: [{
             directory: path.join(__dirname, 'dist'),
             serveIndex: false,
-            watch: false,
+            watch: true,
         }]
         // contentBase: path.join(__dirname, 'dist')
     }
