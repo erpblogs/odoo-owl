0cc4f7f6a01a740ef60019986902d7605f562fea
quang.trinhvan1 <quang.trinhvan1@vti.com.vn>
2024-03-06 17:53:14 +0700

    using bootstrap 5 & sample

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 // const $ = require('jquery')
 
-$('h1').css('background', 'purple')
+// $('h1').css('background', 'purple')
+
+// const bootstrap = require('bootstrap')
+import Carousel from 'bootstrap/js/dist/carousel'
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new Carousel(myCarousel, {
+  interval: 1000,
+  ride: true
+})
