2bf80a37fe421fa7e3f6157aea8004ee7cf6dd8e
quang.trinhvan1 <quang.trinhvan1@vti.com.vn>
2024-03-06 13:42:47 +0700

    start webpack building

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import { __info__ } from "@odoo/owl";
-// import { Root } from "@components/Home/";
-
-
-
-// mount(Root, document.body);
-console.log('Hello World!', __info__.version)
-
 const titleElement = document.querySelector('#title')
-titleElement.style.color = 'red'
+titleElement.style.color = 'green'
