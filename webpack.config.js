be16ff07f02f2df847387ecd439e6cf56a4a4856
quang.trinhvan1 <quang.trinhvan1@vti.com.vn>
2024-03-06 14:12:15 +0700

    guild to setup with nodejs 18

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    }
-  }
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: [{
+            directory: path.join(__dirname, 'dist')
+        }]
+    },
+
 }
